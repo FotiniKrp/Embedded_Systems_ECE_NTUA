602483f4c3f9df7ec80bf51e4ad5ef210c5e1c68
FotiniKrp <134291985+FotiniKrp@users.noreply.github.com>
2025-02-26 22:37:53 +0200

    Delete lab1 directory

--- lab1/script2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-start_time=$(date +%s)
-
-python3 genOptimizer.py
-
-end_time=$(date +%s)
-total_time=$((end_time - start_time))
-echo "Total time for exhaustive search: ${total_time} seconds"
